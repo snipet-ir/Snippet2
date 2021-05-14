a7bc04d6ed30de5d81a73889063d5ac305b17054
Ehsan <ehcan@icloud.com>
2021-05-14 22:00:46 +0430

    Implement jsdoc for user's db

--- db/users.js
+++ db/users.js
@@ -1,6 +1,17 @@
 const { users } = require('./models');
 const argon = require('../services/argon2');
 
+/**
+ * Use our constants to check for what the user chose.
+ * @typedef {{id: ObjectId, username: string, password: string, ban: boolean, otp:{active: boolean, secret: string, backupCodes:[{code: string, active: false}]}}} User
+ */
+
+/**
+ * Create a user
+ * @param {string} username
+ * @param {string} password
+ * @returns {Promise<User>}
+ */
 async function createUser(username, password) {
 	try {
 		return await users.create({ username, password: await argon.hash(password) });
@@ -10,6 +21,10 @@ async function createUser(username, password) {
 	}
 }
 
+/**
+ * @param {string} username
+ * @returns {Promise<User|null>}
+ */
 async function findUserByUsername(username) {
 	try {
 		return await users.findOne({ username: { $regex: new RegExp(username, 'i') } }).lean();
@@ -19,6 +34,10 @@ async function findUserByUsername(username) {
 	}
 }
 
+/**
+ * @param {string} id
+ * @returns {Promise<User|null>}
+ */
 async function findUserByID(id) {
 	try {
 		return await users.findById(id).lean();
