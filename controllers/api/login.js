8ec337bddded2fe4fc167c0ea7d03c0bd5540aaa
Ehsan <ehcan@icloud.com>
2021-05-14 22:01:10 +0430

    Implement login with OTP

--- controllers/api/login.js
+++ controllers/api/login.js
@@ -3,6 +3,7 @@ const jwt = require('../../services/jwt');
 const { users } = require('../../db');
 const argon = require('../../services/argon2');
 const validator = require('../../services/validator');
+const otp = require('../../services/otplib');
 
 async function login(req, res, next) {
 	try {
@@ -17,14 +18,25 @@ async function login(req, res, next) {
 			throw Error('User nout found');
 		}
 
-		if (await argon.verify(foundUser.password, password)) {
-			const token = jwt.getToken({ id: foundUser._id, username });
-			return res.json({ success: true, token, username: foundUser.username });
-		} else {
+		const passwordVerified = await argon.verify(foundUser.password, password);
+		if (!passwordVerified) {
 			throw Error(`username and password didn't match`);
 		}
+
+		if (foundUser.otp.active) {
+			if (!'token' in req.body) {
+				throw Error(`You should provide otp code for login`);
+			}
+
+			const { token } = req.body;
+			if (!otp.verify(foundUser.otp.secret, token)) {
+				throw Error(`OTP Token is invalid`);
+			}
+		}
+
+		const token = jwt.getToken({ id: foundUser._id, username });
+		return res.json({ success: true, token, username: foundUser.username });
 	} catch (err) {
-		console.error(err);
 		next(err);
 	}
 }
