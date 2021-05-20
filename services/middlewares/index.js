12de34e03530d583d3872966c7194839af50b043
Ehsan <ehcan@icloud.com>
2021-05-20 19:44:36 +0430

    Implement local cache

--- services/middlewares/index.js
+++ services/middlewares/index.js
@@ -1,12 +1,24 @@
 const jwt = require('../jwt');
 const { users } = require('../../db');
 const { StatusCodes } = require('http-status-codes');
+const cache = require('../cache');
 
 async function check(req, res, next) {
 	try {
-		let verify = jwt.verify(req.headers.token);
+		const token = req.headers.token;
+
+		// Check in cache
+		const cacheRes = cache.get(token);
+		if (cacheRes) {
+			req.user = cacheRes;
+			return next();
+		}
+
+		// Check from db
+		const verify = jwt.verify(token);
 		if (verify) {
 			req.user = await users.findUserByID(verify.id);
+			cache.set(token, req.user);
 			return next();
 		}
 	} catch (err) {
