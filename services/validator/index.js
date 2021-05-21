4e57ef57407a02a215d98394728e4d4c97dca5cf
Ehsan <ehcan@icloud.com>
2021-05-21 12:40:56 +0430

    Implement login

--- services/validator/index.js
+++ services/validator/index.js
@@ -1,4 +1,19 @@
+const config = require('../../config');
+const schema = require('./schema');
+
+function login(req, res, next) {
+	try {
+		const { error } = schema.login.body.validate(req.body, config.validator.joiConfigs);
+		if (error) {
+			throw Error(error);
+		}
+	} catch (err) {
+		next(err);
+	}
+}
+
 module.exports = {
 	username: require('./username'),
 	password: require('./password'),
+	login,
 };
---
+++ services/validator/schema.js
@@ -0,0 +1,23 @@
+const config = require('../../config');
+const joi = require('joi');
+
+const login = {
+	body: joi.object().keys({
+		username: joi
+			.string()
+			.min(config.validator.username.minLength)
+			.max(config.validator.username.maxLength)
+			.regex(/^\w+$/)
+			.required(),
+		password: joi
+			.string()
+			.min(config.validator.password.minLength)
+			.max(config.validator.password.maxLength)
+			.required(),
+		token: joi.string().required(),
+	}),
+};
+
+module.exports = {
+	login,
+};
