3fe8af4fe04058d8b2bb393cda5a87f6ba34d476
Ehsan <ehcan@icloud.com>
2021-05-21 14:09:41 +0430

    Update js file version

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express();
 const controller = require('../../controllers/views');
-const auth = require('../../services/middlewares');
 
 router.get('/', controller.index);
 router.get('/login', controller.login);
