ae1fd6c5e1136c3f4f803a194722322848ea2bec
Ehsan Seyedi <ehcan@icloud.com>
2022-05-22 21:03:37 +0200

    Read rate limitter from env

--- services/rateLimitter/index.js
+++ services/rateLimitter/index.js
@@ -1,15 +1,15 @@
 const rateLimit = require('express-rate-limit');
-
+const config = require('../../config');
 // 3 request per second for all requests
 const genericLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000,
-	max: 180,
+	max: config.rateLimitter.generic,
 });
 
 // 30 request per minute for other
 const loginLimiter = rateLimit({
 	windowMs: 1 * 60 * 1000,
-	max: 30,
+	max: config.rateLimitter.login,
 });
 
 module.exports = {
